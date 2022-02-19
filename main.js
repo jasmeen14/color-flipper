a40fd50c135d3b2263039902d7b3ccc61006497d
jasmeen14 <jasmeen6199@gmail.com>
2022-02-19 01:07:28 +0100

    created the color-flipper project

---
+++ main.js
@@ -0,0 +1,14 @@
+const colors = ["purple", "yellow", "rgba(134,124,120)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+btn.addEventListener("click",function(){
+    const randomNumber =getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber()
+{
+ return Math.floor( Math.random()* colors.length);
+}
